dd111bb7f55145b5797e6d836f49b951bba0da21
Piran00 <jakob.daanay@gmail.com>
2023-03-25 22:23:21 +0100

    finished version

--- todolist.rb
+++ todolist.rb
@@ -1,7 +1,7 @@
 require 'etc'
 def greet() 
   name = Etc.getlogin
-  puts "\t\t\t\t\t  Welcome to your todo_list #{name}"
+  puts "\t\t\t\t\tWelcome to your todo_list #{name}"
   return name
 end
 def display_tasks()
@@ -30,21 +30,40 @@ def progress_bar()
   precantage_num = pending_tasks / total_tasks
   rest_task = 100 - precantage_num
   puts "\t\t\t\t\t    Youre \e[32m#{rest_task}%\e[0m done"
-  puts "["+ "\e[32m-\e[0m" * rest_task + "#" * precantage_num + "]"
+  print "\n["
+  rest_task.times do
+    print "\e[32m=\e[0m"
+    sleep(0.01)
+  end
+  precantage_num.times do
+    print "#"
+    sleep(0.01)
+  end
+  print "]"
+  if rest_task == 100
+    puts "\n,WOW YOU´RE ALL DONE"
+  end
+
 end
+
 def add_task(task_name)
-  File.open('current_tasks.txt', 'a') do |file|
-    file.write("#{task_name}\n")
+  file_check1 = File.open('total_tasks', 'r').read.split(" ")
+  if file_check1.include? task_name
+    puts "Sorry #{task_name} already exsists"
+  else
+    File.open('current_tasks.txt', 'a') do |file|
+      file.write("#{task_name}\n")
+    end
+    File.open('total_tasks', 'a') do |file|
+      file.write("#{task_name}\n")
+    end
   end
-  File.open('total_tasks', 'a') do |file|
-    file.write("\n#{task_name}")
-  end
+
 end
 def finish_task(task_name)
   file_contents = File.open('current_tasks.txt')
   task_list = file_contents.read.split("\n")
   file_contents.close
-  puts task_list
   if task_list.include? task_name
     task_list.delete(task_name)
     File.write('completed_tasks',"#{task_name}\n",mode: "a" ) 
@@ -54,24 +73,29 @@ def finish_task(task_name)
     end
     puts"ok you just finished #{task_name} good job"
   else
-    puts "this Task dosent exist currently\n do you want to add it y/n"
+    puts "This Task dosent exist would you like to add #{task_name} as a task\ny/n"
     yn = gets.chomp
     if yn == "y"
       add_task(task_name)
+      task_list.delete(task_name)
+      File.write('completed_tasks',"#{task_name}\n",mode: "a" ) 
+      File.write("current_tasks.txt", "")
+      task_list.each do | writee |
+        File.write('current_tasks.txt',"#{writee}\n",mode: "a" ) 
+      end
     end
 
   end
 end
 def reset()
-  File.write("current_tasks.txt", "nothing\n")
-  File.write("total_tasks", "nothing")
+  File.write("current_tasks.txt", "All_done\n")
+  File.write("total_tasks", "All_done \n")
   File.write("completed_tasks", "")
 end
 def un_finish_task(task_name)
   file_contents = File.open('completed_tasks')
   task_list = file_contents.read.split("\n")
   file_contents.close
-  puts task_list
   if task_list.include? task_name
     task_list.delete(task_name)
     File.write('current_tasks.txt',"#{task_name}\n",mode: "a" ) 
@@ -80,12 +104,38 @@ def un_finish_task(task_name)
       File.write('completed_tasks',"#{writee}\n",mode: "a" ) 
     end
     puts"ok you just unfinished #{task_name} "
+  else  
+    puts "This task dose not exsist\nWould you like to add #{task_name} as a task \ny/n"
+    yn = gets.chomp
+    if yn == "y"
+      add_task(task_name)
+    end
   end
 
 end
+def delet_task_from_file(task_name,filename)
+  file_contents = File.open("#{filename}")
+  task_list = file_contents.read.split("\n")
+  task_list.delete(task_name)
+  File.write("#{filename}","",mode: "w" )
+  task_list.each do |writee|
+    File.write("#{filename}","#{writee}\n",mode: "a" ) 
+  end
+end
+def delete_task(task_name)
+  delet_task_from_file(task_name,'current_tasks.txt')
+  delet_task_from_file(task_name,'completed_tasks')
+  file_contents = File.open("total_tasks")
+  task_list = file_contents.read.split("\n")
+  task_list.delete(task_name)
+  File.write("total_tasks","",mode: "w" )
+  task_list.each do | writee |
+    File.write("total_tasks","#{writee}\n",mode: "a" ) 
+  end
+end
 
 def crud()
-  puts"\t\t\t\t\t    what do you want to do?\nadd a task(add)\nfinish a task(fin)\nUn-finish task (unfin)\nReset (re)\nDelete task(del)\nExit (ex)"
+  puts"\n\t\t\t\t\t    what do you want to do?\n\n\nadd a task(add)\nfinish a task(fin)\nUn-finish task (unfin)\nReset (re)\nDelete task(del)\nExit (ex)"
   input = gets.chomp
   action , filename = input.split(" ")
   case action
@@ -98,6 +148,7 @@ def crud()
   when "re"
     reset
   when "del"
+    delete_task(filename)
   when "ex"
     puts"see you next time"
     puts "\e[H\e[2J"
